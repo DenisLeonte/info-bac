b2bc9e3c5d6ad73592f5f3b98119e68ebbfc2dbe
MyBoyDoru <denis.leonte123@gmail.com>
2020-09-20 20:00:23 +0300

    Varianta 1 sub 3 gata, 4 inceput

---
+++ 1/3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    bool found = false;
+    cin>>n;
+    ifstream in("bac.txt");
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        in>>x;
+        if(x % n == 0)
+        {
+            cout<<x<<" ";
+            found = true;
+        }
+    }
+    if(!found)
+        cout<<"NU EXISTA";
+}
